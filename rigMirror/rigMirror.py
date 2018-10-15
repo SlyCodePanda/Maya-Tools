10d935e441405f2606cebd9dccec80d893527093
Renee Marsland <reneemarsland@hotmail.com>
2018-10-15 21:15:57 +1030

    Update rigMirror/rigMirror.py
    adding better functionality for mirroring rig controls.

--- rigMirror/rigMirror.py
+++ rigMirror/rigMirror.py
@@ -1,4 +1,5 @@
 from maya import cmds
+import pymel.core
 
 # A class that holds all functions for mirroring a rig.
 class rigMirror(object):
@@ -14,9 +15,29 @@ class rigMirror(object):
         self.newSide = newSide
 
         # Get the rig controls based on user selection.
-        self.controls = cmds.ls(sl=True)
+        # Get both groups and any children.
+        self.controls = cmds.ls(sl=True, dag=True, type="transform")
+
+        print("CONTROLS : \n %s" % self.controls)
 
         self.mirrorControls()
+
+    # Checks if node is a group.
+    # Returns True if node is a group, False otherwise.
+    def isGroup(self, node):
+        if cmds.nodeType(node) != "transform":
+            return False
+
+        children = cmds.listRelatives(node, c=True)
+
+        if children == None:
+            return True
+
+        for c in children:
+            if cmds.nodeType(c) != 'transform':
+                return False
+        else:
+            return True
 
     # Class functions
     # Takes in an object and gives it a new name whether you are creating a left or right duplicate of it.
@@ -26,21 +47,40 @@ class rigMirror(object):
 
         return newName
 
-    # Get all the controls and their groups on one side of the rig and mirror them to the other side.
+    # Duplicates and mirrors rig controls
     def mirrorControls(self):
-        # Iterate through all the controls and their groups and duplicate them
+        # Iterate through the controls and their groups and duplicate them.
         for control in self.controls:
             # Get the group and the control in it.
+            # If selection is a group, get ctrlGrp.
+            duplicate = ""
             ctrlName = str(control)
-            ctrlGrps = cmds.listRelatives(ctrlName, parent=True)
-            transX = cmds.getAttr(ctrlGrps[0] + ".translateX")
-            transY = cmds.getAttr(ctrlGrps[0] + ".translateY")
-            transZ = cmds.getAttr(ctrlGrps[0] + ".translateZ")
 
-            # Duplicate groups and controls
-            duplicate = cmds.duplicate(ctrlGrps[0], name=self.getNewSideName(ctrlGrps[0]))
-            # Mirror the duplicate to other side.
-            cmds.xform(duplicate[0], t=(-transX, transY, transZ))
+            grpName = cmds.listRelatives(ctrlName, parent=True)
+
+            # If the object selected is a control with a group.
+            if grpName != None:
+                grp = cmds.listRelatives(ctrlName, parent=True)[0]
+                # Duplicate groups and controls
+                duplicate = cmds.duplicate(ctrlName, rr=True, name=self.getNewSideName(ctrlName))
+                newGrp = cmds.group(duplicate, name=self.getNewSideName(grp), world=True)
+
+            # Scale the group at -1 in axis you want to mirror. For now we will just do it in X-axis.
+            if duplicate:
+                group = cmds.listRelatives(duplicate, parent=True)
+                cmds.group(group, name="ctrls_mirrorGrp")
+                cmds.xform(group, os=True, piv=[0, 0, 0])
+                cmds.scale(-1, 1, 1, group)
+                cmds.ungroup("ctrls_mirrorGrp")
+
+                # Freeze transforms.
+                cmds.makeIdentity(group, apply=True, t=1, r=1, s=1, n=0)
+
+                # Clear construction history.
+                cmds.delete(group, constructionHistory=True)
+
+                # Re-center pivot
+                cmds.xform(group, centerPivots=True)
 
     # Get all the connections and store them in connections dictionary. Then set up connections on the other side.
     def mirrorConnections(self):
