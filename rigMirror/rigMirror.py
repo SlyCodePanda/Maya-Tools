aeb6bc4f0ecbf42a992fe4de2724c5518a02d40e
Renee Marsland <reneemarsland@hotmail.com>
2018-11-07 18:19:39 +1030

    Update rigMirror/rigMirror.py
    Fixed weird FK duplication error and added functionality to duplicating pole vector constraint to PV shape.

--- rigMirror/rigMirror.py
+++ rigMirror/rigMirror.py
@@ -1,5 +1,12 @@
 from maya import cmds
 
+'''
+    Order of steps to take when using this tool :
+        STEP 1 : Mirror FK
+        STEP 2 : Mirror IK
+        STEP 3 : Mirror Pole Vector
+'''
+
 # A class that holds all functions for mirroring a rig.
 class rigMirror(object):
     # Class variables
@@ -16,6 +23,10 @@ class rigMirror(object):
     joints = []
     dupJoints = []
 
+    # POLE VECTOR
+    poleVector = []
+    dupPoleVector = []
+
     # IK
     ikHandle = []
     dupIkHandle = []
@@ -29,9 +40,6 @@ class rigMirror(object):
         self.newSide = newSide
         # Get what axis we are mirroring along.
         self.scale = scale
-
-        print("CONTROLS : \n" + str(self.FK_controls))
-        print("JOINTS : \n" + str(self.joints))
 
     ################################
     # BASIC HELPER FUNCTIONS
@@ -68,18 +76,16 @@ class rigMirror(object):
     # Mirror the FK rig (joints and controls)
     def mirrorFK(self):
         # Get the rig controls based on user selection.
-        self.FK_controls = cmds.ls(sl=True, dag=True, type="transform")
+        self.FK_controls = cmds.ls(sl=True, et="transform")
         self.joints = cmds.ls(sl=True, dag=True, type="joint")
 
-        # Strip joints from controls list.
-        for cur in self.FK_controls:
-            if cmds.objectType(cur) == "joint":
-                self.FK_controls.remove(cur)
+        print("FK_controls : " + str(self.FK_controls))
+        print("joints : " + str(self.joints))
 
         # Duplicate and mirror rig controls from one side to the other.
         self.mirrorControls("FK")
         # Duplicate and mirror joints from one side to the other.
-        self.mirrorJoints()
+        self.mirrorJoints("FK")
 
     # Mirror the IK rig (joints and controls)
     def mirrorIK(self):
@@ -88,22 +94,11 @@ class rigMirror(object):
         self.IK_controls = cmds.ls(sl=True, et="transform")
         self.joints = cmds.ls(sl=True, dag=True, type="joint")
 
-        # Strip non controls from controls list.
-        # for cur in self.IK_controls:
-        #     if cmds.objectType(cur) == "joint" \
-        #             or cmds.objectType(cur) == "ikEffector" \
-        #             or cmds.objectType(cur) == "ikHandle":
-        #         self.IK_controls.remove(cur)
-
-        print("IK handle: " + str(self.ikHandle))
-        print("IK controls: " + str(self.IK_controls))
-        print("Joints: " + str(self.joints))
-
         # Duplicate and mirror rig controls from one side to the other.
         self.mirrorControls("IK")
 
         # Duplicate and mirror joints from one side to the other.
-        self.mirrorJoints()
+        self.mirrorJoints("IK")
 
         # Duplicate and mirror Ik handle.
         self.mirrorIKhandle()
@@ -150,7 +145,7 @@ class rigMirror(object):
         cmds.ungroup("ctrls_mirrorGrp")
 
     # Duplicates and mirrors the skeleton (joints).
-    def mirrorJoints(self):
+    def mirrorJoints(self, type):
         # Duplicate top level joint in hierarchy and rename it for new side.
         # If there are no joints selected, return out of function and do nothing.
 
@@ -158,6 +153,7 @@ class rigMirror(object):
             if self.scale == "X":
                 self.dupJoints = cmds.mirrorJoint(self.joints[0], mirrorYZ=True, mirrorBehavior=True,
                                        sr=[self.curSide, self.newSide])
+            # TODO : Add other axis to list for scaling (Y, Z)
         else:
             return
         constraints = []
@@ -165,39 +161,55 @@ class rigMirror(object):
         ctrls = []
 
         # Add duplicated controls to new list (without the groups).
-        for i in range(len(self.FK_dupControls)):
-            if not self.isGroup(self.FK_dupControls[i]):
-                ctrls.append(self.FK_dupControls[i])
+        if type == "FK":
+            for i in range(len(self.FK_dupControls)):
+                if not self.isGroup(self.FK_dupControls[i]):
+                    ctrls.append(self.FK_dupControls[i])
+
+        elif type == "IK":
+            for i in range(len(self.IK_dupControls)):
+                if not self.isGroup(self.IK_dupControls[i]):
+                    ctrls.append(self.IK_dupControls[i])
 
         print("CTRLS : " + str(ctrls) )
 
-        # Add old parent contraints from the duplicates list to new list, and add joints to another new list.
+        # Add old parent constraints from the duplicates list to new list, and add joints to another new list.
         for i in range(len(self.dupJoints)):
             if cmds.objectType(self.dupJoints[i]) == "parentConstraint":
                 constraints.append(self.dupJoints[i])
             else:
                 joints.insert(0, self.dupJoints[i])
 
-        # Parent constraint new side controls to right new joints.
-        for i in range(len(ctrls)):
-            curJoint = joints[i]
-            oldCon = constraints[i]
-            # Remove old constraint and add new one for new side.
-            # print("Does constraint exist? : " + str(cmds.parentConstraint(oldCon, query=True, name=True)))
-            cmds.parentConstraint(curJoint, oldCon, edit=True, rm=True)
-            cmds.parentConstraint(ctrls[i], curJoint, mo=True)
+        # If FK, parent constraint new side controls to right new joints.
+        if type == "FK":
+            for i in range(len(ctrls)):
+                curJoint = joints[i]
+                oldCon = constraints[i]
+                # Remove old constraint and add new one for new side.
+                cmds.parentConstraint(curJoint, oldCon, edit=True, rm=True)
+                cmds.parentConstraint(ctrls[i], curJoint, mo=True)
 
     # Mirror the pole vector.
     def mirrorPoleVector(self):
         # Duplicated selected pole vector control
-        selected = cmds.ls(sl=True)
-        dup = cmds.duplicate(selected, name=self.getNewSideName(selected[0]))
-        topGrp = dup[0]
+        self.poleVector = cmds.ls(sl=True)
+        self.dupPoleVector = cmds.duplicate(self.poleVector, name=self.getNewSideName(self.poleVector[0]))
+        topGrp = self.dupPoleVector[0]
         topGrp.replace(topGrp[-1], "")
 
+        children = cmds.listRelatives(topGrp, children=True, ad=True, type="transform", f=True)
+
+        # Go through children list and rename them to the other side.
+        for i in range(len(children)):
+            if cmds.objectType(children[i]) != 'joint':
+                name = children[i].split("|")[-1]
+                newName = cmds.rename(children[i], self.getNewSideName(name))
+                # Adding new duplicated controls to global list 'dupControls'.
+                self.dupPoleVector.append(newName)
+
+
         # Create a temporary group to use to scale the group in -1 along the X axis.
-        # May add functionality to choose which axis to scale across later.
-        group = cmds.group(dup[0], name="ctrls_mirrorGrp")
+        group = cmds.group(self.dupPoleVector[0], name="ctrls_mirrorGrp")
         cmds.xform(group, os=True, piv=[0, 0, 0])
         if self.scale == "X":
             cmds.scale(-1, 1, 1, group)
@@ -207,30 +219,41 @@ class rigMirror(object):
             cmds.scale(1, 1, -1, group)
         cmds.ungroup("ctrls_mirrorGrp")
 
+        # Pole vector constraint new pole vector control to new ikhandle
+        # Get original pole vector shape node.
+        pvChildren = cmds.listRelatives(self.poleVector, children=True, ad=True, type="transform", f=True)
+        child = pvChildren[0].split("|")[-1]
+        # Get the original pv shape's constraints.
+        pvCon = cmds.listConnections(str(child), type="poleVectorConstraint", d=True, s=False)[0]
+        # Get the IK
+        Lf_ik = cmds.listConnections(pvCon, type="ikHandle")[0]
+
+        if Lf_ik:
+            Rt_ik = self.getNewSideName(str(Lf_ik))
+            Rt_pv = self.dupPoleVector[-1]
+            cmds.poleVectorConstraint(str(Rt_pv), str(Rt_ik))
+
+        #pvConstraint = cmds.listConnections(str(self.poleVector[0]), d=True, s=False, type="poleVectorConstraint")[0]
+        #conType = cmds.objectType(pvConstraint)
+        # if pvConstraint:
+        #     print("PV CONSTRAINT FOUND")
+        #cmds.poleVectorConstraint(str(self.dupPoleVector[0]), str(self.dupIkHandle))
+
     # Mirror IK handle.
     def mirrorIKhandle(self):
         # Strip out any non-joints from the dupJoints list.
         self.dupJoints = cmds.ls(self.dupJoints, type="joint")
 
-        # Joints to use for the ikhandle.
+        # Joints to use for the ikhandle. THROW AWAY.
         topJoint = self.dupJoints[0]
         bottomJoint = self.dupJoints[-1]
-
-        print(topJoint)
-        print(bottomJoint)
 
         # Create new ikhandle and change the targets from left shoulder and wrist to right shoulder and wrist.
         cmds.ikHandle(self.getNewSideName(self.ikHandle[0]), e=True, sj=self.dupJoints[0], ee=self.dupJoints[-1])
 
-        print("Old IK: " + str(self.ikHandle))
-        print("New Ik" + str(self.dupIkHandle))
-
         # point constraint IK handle to to foot/hand ctrl. (ik then foot/hand selection order)
         ik = cmds.ls(self.getNewSideName(self.ikHandle[0]))[0]
         ctrl = self.IK_dupControls[0]
-        print("IK : " + str(ik))
-        print("FOOT : " + str(ctrl))
         cmds.pointConstraint(ctrl, ik)
 
-        # TODO: pole vector constraint pole vector control to ikhandle.
 
