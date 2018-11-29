5d7850b4b10e25f3db4e706b791e8ab355d9255b
Renee Marsland <reneemarsland@hotmail.com>
2018-11-29 14:55:21 +1030

    Adding new features

--- rigControlBuilder/rigControlBuilder.py
+++ rigControlBuilder/rigControlBuilder.py
@@ -2,13 +2,19 @@ from maya import cmds
 import pymel.core as pm
 
 '''
-Steps for creating control.
-1. Create shape. X
-2. Orient the shape in the right axis using setattr. X
-3. Group the control to itself and rename. X
-4. Select shape control group and the joint it is going to control and parent them. X
-5. Zero out translations and rotations on group. X
+Steps taken to create control(s).
+1. Create shape.
+2. Orient the shape in the right axis using setattr.
+3. Group the control to itself and rename.
+4. Select shape control group and the joint it is going to control and parent them.
+5. Zero out translations and rotations on group.
 6. Un-parent joint and control group.
+7. Parent controls under each-other if checkbox is set to True.
+8. Parent constraint joints to controls.
+
+* Add colour picker to change the shapes colour.
+* Give option to add to new layer?
+* Ask if the shape being built is a pole vector?
 '''
 
 
@@ -24,7 +30,7 @@ class controlBuilder(object):
 
     windowName = "RigControlBuilder"
     scriptName = 'controlBuilder'
-    height = 80
+    height = 100
     width = 400
     xVal = 0
     yVal = 0
@@ -52,7 +58,7 @@ class controlBuilder(object):
         cmds.deleteUI(self.windowName)
 
     def buildUI(self):
-        textWidth = 80
+        textWidth = 90
         buttonWidth = 197
 
         cmds.setParent(self.window)
@@ -77,6 +83,15 @@ class controlBuilder(object):
 
         cmds.setParent('..')
 
+        # Select whether you want to parent the chain of controls and their groups.
+        cmds.rowLayout(numberOfColumns=3)
+        cmds.checkBox('parentControls_checkBox', label="Parent controls under each other")
+
+        # Select whether the control we are building is intended as a Pole Vector control.
+        cmds.checkBox('poleVector_checkBox', label="Pole Vector")
+
+        cmds.setParent('..')
+
         # Create shape.
         cmds.rowLayout(numberOfColumns=2)
         pm.button(label="Create", align='right', command=self.createShapes, width=buttonWidth)
@@ -96,20 +111,42 @@ class controlBuilder(object):
         # List of new shapes created.
         newShapes = []
 
+        # Dictionaries
         jointShapes = {}
         jointGroups = {}
 
+        # List of items that will be used to group under each other.
+        controlAndGroup = []
+
         print(str(self.joints))
 
+        # TODO : Need to figure out a way to only allow for unique names to be created.
+
         # Create controls for all the joints depending on which radio button is selected.
+        # Currently outputs an error when trying to create multiple controls at the same joint.
         for joint in self.joints:
             if shapeType == 1:
                 name = joint.split('_')[0] + "_ctrl"
+                # Check if name already exists.
+                if cmds.objExists(name):
+                    cmds.error("Trying to create a shape with the same name."
+                               "Please change the name of previously built shape..")
+                    return
+                # Checks if this control should be a pole vector naming convention.
+                elif cmds.checkBox('poleVector_checkBox', q=True, value=True):
+                    name = "poleVector_ctrl"
                 self.pickShape('Circle', name)
                 newShapes.append(name)
                 jointShapes.update({joint : name})
             elif shapeType == 2:
                 name = joint.split('_')[0] + "_ctrl"
+                if cmds.objExists(name):
+                    cmds.error("Trying to create a shape with the same name. "
+                               "Please change the name of previously built shape..")
+                    return
+                    # Checks if this control should be a pole vector naming convention.
+                elif cmds.checkBox('poleVector_checkBox', q=True, value=True):
+                    name = "poleVector_ctrl"
                 self.pickShape('Box', name)
                 newShapes.append(name)
                 jointShapes.update({joint: name})
@@ -120,14 +157,23 @@ class controlBuilder(object):
         self.zVal = float(cmds.textField("zOrient", q=True, text=True))
 
         for shape in newShapes:
-            cmds.setAttr(shape + '.rotateX', self.xVal)
-            cmds.setAttr(shape + '.rotateY', self.yVal)
-            cmds.setAttr(shape + '.rotateZ', self.zVal)
+            if self.xVal != 0.0:
+                cmds.setAttr(shape + '.rotateX', self.xVal)
+            elif self.yVal != 0.0:
+                cmds.setAttr(shape + '.rotateY', self.yVal)
+            elif self.zVal != 0.0:
+                cmds.setAttr(shape + '.rotateZ', self.zVal)
 
         # Group and rename.
         for key, value in jointShapes.items():
             cmds.group(value, n=value + 'Grp')
             jointGroups.update({key : value + 'Grp'})
+            # Add the control and it's group to a list in the order that we will need to group them.
+            controlAndGroup.insert(0, value +'Grp')
+            controlAndGroup.insert(0, value)
+
+        # Check list of group and control items.
+        print(controlAndGroup)
 
         # Parent, zero out translations and rotations, and un-parent.
         for key, value in jointGroups.items():
@@ -140,6 +186,25 @@ class controlBuilder(object):
             cmds.setAttr(value + '.rotateZ', 0)
             cmds.parent(value, world=True)
 
+        # Parent controls and groups under each other.
+        # Iterate through the list and group the items up the chain IF the checkbox is on.
+        if cmds.checkBox('parentControls_checkBox', q=True, value=True):
+            for index in range(len(controlAndGroup)):
+                if self.isGroup(controlAndGroup[index]) and index != len(controlAndGroup)-1:
+                    # print("Group " + controlAndGroup[index] + " to " + controlAndGroup[index+1])
+                    cmds.parent(controlAndGroup[index], controlAndGroup[index+1])
+
+            # Parent constraint control to joints IF checkbox is on.
+            print("Control and Joint Pairs : ")
+            print(jointShapes)
+            for key, value in jointShapes.items():
+                cmds.parentConstraint(value, key)
+
+    ########################
+    # Helper functions
+    ########################
+
+    # Returns the right curve based on the shape name passed, also names it according to name passed.
     def pickShape(self, shape, name):
         if shape == 'Circle':
             # Circle shape control.
@@ -154,3 +219,18 @@ class controlBuilder(object):
                               k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
             return box
 
+    # Returns True if node is a group, False otherwise.
+    def isGroup(self, node):
+        if cmds.nodeType(node) != "transform":
+            return False
+
+        children = cmds.listRelatives(node, children=True)
+
+        if children is None:
+            return True
+
+        for child in children:
+            if cmds.nodeType(child) != 'transform':
+                return False
+        else:
+            return True
