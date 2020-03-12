ce46483fc9f63b5b53abd47b56c82abb08885789
Renee Marsland <reneemarsland@hotmail.com>
2020-03-12 11:18:32 +1030

    Update ngonFinder.py
    final ngon finder script.

--- ngonFinder.py
+++ ngonFinder.py
@@ -2,21 +2,23 @@ import maya.cmds
 
 """
 Looks at the selected mesh and selects any faces that are ngons
-(has more than 4 edges on a face).
+(has more than 4 edges on a face/more than 2 triangles making up the face).
 
+refs used:
 https://stackoverflow.com/questions/32428452/selecting-faces-in-a-list-maya-python
 """
 
 
 def analyzeFace(faces):
     """
-    ahsdfkjasf
-    :param faces:
-    :return:
+    Looks through the obj faces given and selects all the ngons in the list.
+    :param faces: faces we want to look through for ngons.
+    :return: Returns 'None' if no ngons are found.
     """
     # Expand compacted face name into a list of face names.
     facesList = maya.cmds.filterExpand(faces, sm=34)
     ngonCount = 0
+    ngonFaces = []
 
     # Clear selection.
     maya.cmds.select(cl=True)
@@ -25,22 +27,33 @@ def analyzeFace(faces):
     for face in facesList:
         maya.cmds.select(face, add=True)
         numOfTris = maya.cmds.polyEvaluate(tc=True)
+        # deselect face.
+        maya.cmds.select(face, d=True)
 
         if numOfTris > 2:
             ngonCount += 1
-            print "ngon on ", face
+            ngonFaces.append(face)
 
-        else:
-            # deselect face.
-            maya.cmds.select(face, d=True)
+    # If list is empty (no ngons) return None.
+    if len(ngonFaces) == 0:
+        objName = facesList[0].split('.')[0]
+        print "No ngons found on ", objName
+        return None
+
+    # Select the faces in the ngon list.
+    [maya.cmds.select(face, add=True, af=True) for face in ngonFaces]
 
     print "NGons found: ", ngonCount
 
 
 def run():
-    selObjs = maya.cmds.ls(selection=True)
+    selObj = maya.cmds.ls(selection=True)
 
-    for obj in selObjs:
-        faces = maya.cmds.ls('%s.f[*]' % obj)
-        print "Object Name: ", selObjs
-        analyzeFace(faces)
+    if len(selObj) > 1:
+        maya.cmds.warning("Please only select one object.")
+        return None
+
+    faces = maya.cmds.ls('%s.f[*]' % selObj[0])
+    print "Object Name: ", selObj[0]
+    analyzeFace(faces)
+
